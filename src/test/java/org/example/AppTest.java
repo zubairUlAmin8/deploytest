e33361977cd4de1920e8d0574064b466f2d06c84
Zubair Ul Amin <zubairulamin8@gmail.com>
2023-08-11 16:36:12 +0500

    ready for pipeline

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -5,7 +5,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Unit test for simple App.
@@ -15,19 +21,29 @@ public class AppTest
 {
 
     @Test
-    public void testApp()
-    {
-
-        WebDriverManager.chromedriver().setup();
+    public void testApp() throws MalformedURLException, InterruptedException {
+//        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--window-size=1920,1200","--ignore-certificate-errors");
 
-        WebDriver driver = new ChromeDriver(options);
+
+//        options.setBrowserVersion("116");
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setBrowserName("chrome");
+
+//        WebDriver driver = new ChromeDriver(desiredCapabilities);
+        WebDriver driver = new RemoteWebDriver(new URL("http://54.252.61.87:4444"), desiredCapabilities);
+
         driver.get("https://urdu.arynews.tv/");
+//        Thread.sleep(5000);s
         String tittle = driver.getTitle();
         if (!tittle.equalsIgnoreCase("")) {
             System.out.println("Website has been opened: "+tittle);
         }
-        driver.close();
+        Assert.assertEquals("5","5");
+
+        driver.quit();
     }
 }
+
+
